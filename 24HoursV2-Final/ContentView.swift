304c1274fc1f1e7abcead4362e2aca6e6d8c2296
Hybrid Theory <jaysinmyers1@gmail.com>
2023-11-16 10:54:57 -0500

    Initial Commit

---
+++ 24HoursV2-Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  24HoursV2-Final
+//
+//  Created by Jaysin Myers on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 24HoursV2-Final/_4HoursV2_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4HoursV2_FinalApp.swift
+//  24HoursV2-Final
+//
+//  Created by Jaysin Myers on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct _4HoursV2_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
