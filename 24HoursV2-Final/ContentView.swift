a2d19da80a4724a15de1f20d8b183bde44b2de6e
Hybrid Theory <jaysinmyers1@gmail.com>
2023-11-16 11:47:17 -0500

    tabview

--- 24HoursV2-Final/ContentView.swift
+++ 24HoursV2-Final/ContentView.swift
@@ -1,29 +1,36 @@
-//
-//  ContentView.swift
-//  24HoursV2-Final
-//
-//  Created by Jaysin Myers on 11/16/23.
-//
-
 import SwiftUI
 
+// Use the relevant folders and swift files to code your chapter of the story. Keep ContentView as-is, unless you want a different type of navigation in your story.
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            Text("textFigma")
-            
-            
-            
+        TabView {
+            CoverView()
+                .tabItem {
+                    Label("24 Hours", systemImage: "m.circle")
+                }
+            Chapter1View()
+                .tabItem {
+                    Label("Chapter 1", systemImage: "1.circle")
+                }
+            Chapter2View()
+                .tabItem {
+                    Label("Chapter 2", systemImage: "2.circle")
+                }
+            Chapter3View()
+                .tabItem {
+                    Label("Chapter 3", systemImage: "3.circle")
+                }
+            Chapter4View()
+                .tabItem {
+                    Label("Chapter 4", systemImage: "4.circle")
+                }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+//
+//
+
